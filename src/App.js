cabee78b489afb65e73e1967825ca67012bc5bd2
negasi-dev <negasihaile19@gmail.com>
2024-10-15 14:32:24 +0300

    Added: Project initial setup

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-container">
+        <div className="app-header">
+          <p className="main-header">
+            <b>AGP Report:</b> Continuous Glucose Monitoring
+          </p>
+        </div>
+
+        <div className="agp-container">
+          <div className="flex justify-between items-center">
+            <div className="text-gray-500">
+              <p className="font-bold">From: 2015-03-19 - 2015-04-02 14 days</p>
+              <p>15 minutes glucose readings</p>
+            </div>
+
+            <select value="">
+              <option value="patient1">Patient 1</option>
+              <option value="patient1">Patient 2</option>
+              <option value="patient1">Patient 3</option>
+              <option value="patient1">Patient 4</option>
+              <option value="patient1">Patient 5</option>
+            </select>
+          </div>
+
+          <div className="flex justify-between space-x-3">
+            {/* TIME IN RANGES */}
+            <div className="w-[55%]">
+              <div className="card">
+                <div className="card-header">
+                  Time In Ranges
+                  <small className="text-sm font-normal ml-2">
+                    Goals for Type 1 and Type 2 Diabetes
+                  </small>
+                </div>
+                <div className="card-body">
+                  {/* TIME IN RANGE CHART HERE */}
+                </div>
+              </div>
+            </div>
+
+            {/* Glucose METRICS */}
+            <div className="space-y-3 w-[55%]">
+              {/* TEST PATIENT  */}
+
+              <div className="card">
+                <div className="card-header">Glucose Metrics</div>
+                <div>{/* GLUCOSE METRICS */}</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">Ambulatory Glucose Profile (AGP)</div>
+            <div className="card-body">
+              AGP is a summary of glucose values from the report period, with
+              median (50%) and other percentiles shown as if they occurred in a
+              single day.
+              {/* AGP REPORT CHART */}
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">Daily Glucose Profiles</div>
+            <div className="card-body">
+              Each day represents a mindnight-to-midnight period.
+              {/* Daily Glucose Profiles CHART */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/colors.js
@@ -0,0 +1,77 @@
+export const tirColors = {
+  above_250: "rgb(242,137,66)",
+  above_180: "rgb(251,192,4)",
+  btwn_70_180: "rgb(87,175,83)",
+  below_70: "rgb(229,43,23)",
+  below_25: "rgb(156,32,37)",
+  gray: "rgb(120,120,125)",
+  balck: "rgb(0, 0, 0)",
+};
+
+// default font of the chart
+export const colors = {
+  above_250_shade: "rgb(251,227,209)",
+  above_250_bold: "rgb(246,180,129)",
+  above_250_line: "rgb(230,123,42)",
+
+  above_180_shade: "rgb(255,235,180)",
+  above_180_bold: "rgb(255,217,122)",
+  above_180_line: "rgb(254,162,2)",
+
+  target_range_shade: "rgb(220,234,215)",
+  target_range_bold: "rgb(166,207,152)",
+  target_range_line: "rgb(52,141,44)",
+
+  below_70_shade: "rgb(240,130,98)",
+  below_70_bold: "rgb(230,43,23)",
+  below_70_line: "rgb(254,66,8)",
+
+  below_54_shade: "rgb(154,34,36)",
+  below_54_bold: "rgb(76,9,8)",
+  below_54_line: "rgb(65,7,5)",
+
+  gray: "rgb(165,163,165)",
+  black: "rgba(0,0,0)",
+
+  // Orange colors to be used when value >250
+  orange: "rgb(246,181,129)",
+  orange_500: "rgb(246,181,129, 1)",
+  orange_400: "rgb(246,181,129, 0.9)",
+  orange_300: "rgb(246,181,129, 0.7)",
+  orange_200: "rgb(246,181,129, 0.5)",
+
+  // Yellow colors to be used between value >180 <=250
+  yellow: "rgb(254,153,0)",
+  yellow_500: "rgbA(254,153,0, 1)",
+  yellow_400: "rgbA(254,153,0, 0.7)",
+  yellow_300: "rgb(254,223,142)",
+  yellow_200: "rgb(255,235,180)",
+
+  // Green colors to be used between value >=70 <=180
+  green: "rgb(54,142,46)",
+  green_500: "rgb(67,146,63)",
+  green_400: "rgb(80,184,72)",
+  green_300: "rgb(172,219,169)",
+  green_200: "rgb(203,234,200)",
+  green_100: "rgba(179, 255, 203, 0.3)",
+
+  // Red colors to be used between value >=54 <70
+  red: "rgb(237,28,36)",
+  red_500: "rgb(237,28,36,1)",
+  red_400: "rgb(237,28,36,0.7)",
+  red_300: "rgb(237,28,36,0.5)",
+  red_200: "rgb(237,28,36,0.3)",
+
+  // Dark red colors to be used when value <54>
+  darkRed: "rgb(155,33,38,1)",
+  darkRed_500: "rgba(155,33,38,1)",
+  darkRed_400: "rgba(155,33,38,0.7)",
+  darkRed_300: "rgba(155,33,38,0.5)",
+  darkRed_200: "rgba(155,33,38,0.3)",
+
+  // Other color
+  grayLine: "rgba(166,166,166, 0.5)",
+  grayText: "rgb(148,163,184)",
+  grayBg_300: "rgba(148,163,184, 0.2)",
+  grayBg_200: "rgba(148,163,184, 0.05)",
+};
